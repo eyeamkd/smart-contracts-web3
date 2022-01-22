61ea3fdafd5401a076d678cd4e7c1d7a11228ae8
Kunal Dubey <kunaldubey2297@gmail.com>
2022-01-23 04:10:13 +0530

    add hoi feature

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol"; 
 
 contract WavePortal{
+    
+    uint256 totalHois; //called state variable, stored permanently 
+
     constructor(){
         console.log("Smart Contract, here"); 
+    } 
+
+    function hoi() public{ 
+        totalHois+=1;
+        console.log("%s has Hoied you", msg.sender);//The msg object comes in built in by whoever calls it
+    }  
+
+    function getTotalHois() public view returns(uint256){
+        console.log("We had a total of %d number of Hois",totalHois);
+        return totalHois; 
     }
 }
---
+++ scripts/run.js
@@ -0,0 +1,34 @@
+const main = async () => {  
+    const [owner,randomPerson] = await hre.ethers.getSigners(); 
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); //we get the hre object on the fly from the hardhat execution environment
+    const waveContract = await waveContractFactory.deploy(); 
+    await waveContract.deployed(); 
+    console.log("Contract deployed to: ", waveContract.address);  
+    console.log("Contract deployed by:", owner.address); 
+    
+    let hoiCount = await waveContract.getTotalHois(); 
+    let waveTransaction = await waveContract.hoi();  
+    await waveTransaction.wait(); 
+
+    hoiCount = await waveContract.getTotalHois(); 
+
+    waveTransaction = await waveContract.connect(randomPerson).hoi();
+    await waveTransaction.wait();
+    
+    hoiCount = await waveContract.getTotalHois();
+
+}; 
+
+
+const runMain = async () => {
+    try {
+        await main(); 
+        process.exit(0); 
+    } catch (error) {
+        console.log(error); 
+        process.exit(1); 
+    }
+} 
+
+runMain(); 
+
